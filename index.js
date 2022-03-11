c62229d56d08297a5141c24b24aeb50cfa24d056
milosn99 <48392694+milosn99@users.noreply.github.com>
2022-03-11 22:14:26 +0100

    sesija

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const cookieSession = require("cookie-session");
 const app = express();
 require("./config/server-config")(app);
 
-// app.use(
-//   cookieSession({
-//     maxAge: 24 * 60 * 60 * 1000,
-//     keys: ["cookieKey"],
-//   })
-// );
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: ["cookieKey"],
+  })
+);
 
 const port = process.env.PORT || 3000;
 const dbUrl = process.env.DB_URL;
